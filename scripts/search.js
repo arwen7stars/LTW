3d00f9103c1e759356ec4b624cc5a1338bd80bd7
tirafesi <tirafesi@hotmail.com>
2016-12-15 20:13:11 +0000

    something

--- scripts/search.js
+++ scripts/search.js
@@ -8,18 +8,33 @@
  */
 var restaurantOnMouseEnter = function(event) {
 
-  var restarauntLink = event.target.href;
+  // get url
+  var restaurantLink = new URL(event.target.href);
+
+  // get search params and get id
+  var restaurantId = restaurantLink.searchParams.get('id');
 
 	// fetch restaurant display divs
 	var restaurantName = $(".restaurant-name");
+  var restaurantDesc = $(".restaurant-desc");
+  var restaurantImg = $(".restaurant-img");
 
 	// update it with new info
 	$.ajax({
 		type: "get",
 		url: "scripts/search_hover.php",
-    data: {"link": restarauntLink}
+    dataType: 'json',
+    data: {"id": restaurantId}
 	}).done(function(data) {
-		console.log(data);
+    if (data[1].score != null) {
+      restaurantName.html(data[0].name + '<br />Score: ' + data[1].score + '/10');
+    } else {
+      restaurantName.html(data[0].name + '<br />No score available yet!');
+    }
+    if (data[2].url != null) {
+      restaurantImg.html('<img src="' + data[2].url + '">');
+    }
+    restaurantDesc.html(data[0].desc);
 	});
 };
 
@@ -28,7 +43,14 @@ var restaurantOnMouseEnter = function(event) {
  * Handler for when the user hovers a restaurant list item
  */
 var restaurantOnMouseLeave = function(event) {
-	console.log("mouse left");
+  // fetch restaurant display divs
+	var restaurantName = $(".restaurant-name");
+  var restaurantDesc = $(".restaurant-desc");
+  var restaurantImg = $(".restaurant-img");
+
+  restaurantName.html("");
+  restaurantDesc.html("");
+  restaurantImg.html("");
 };
 
 
